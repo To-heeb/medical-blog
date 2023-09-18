de4c8fcea3f59300304c5022c6f9a2013d62395e
Oyekola Toheeb <toheeb.olawale.to23@gmail.com>
2023-09-18 23:39:03 +0100

    I am back my beloved php, added laravel sail for docker use on local

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,9 +5,11 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
     /**
      * Seed the application's database.
      */
